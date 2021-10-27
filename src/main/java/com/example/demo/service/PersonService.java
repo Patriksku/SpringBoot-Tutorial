4bd50f97ebc1013e45347f5651a475e2aa22a169
fourseven <patrikskuza@gmail.com>
2021-10-27 12:55:15 +0200

    basic Spring Boot project

---
+++ src/main/java/com/example/demo/service/PersonService.java
@@ -0,0 +1,46 @@
+package com.example.demo.service;
+
+import com.example.demo.dao.PersonDao;
+import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+// Injection will be taking place.
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    // We are autowiring into the specified interface by name.
+    // fakeDao points to FakePersonDataAccessService.
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.insertPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+
+    public int deletePerson(UUID id) {
+        return personDao.deletePersonById(id);
+    }
+
+    public int updatePerson(UUID id, Person newPerson) {
+        return personDao.updatePersonById(id, newPerson);
+    }
+}
